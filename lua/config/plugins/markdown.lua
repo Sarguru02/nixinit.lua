7f40c70c1d1277e1bd7a5c31c3822a2f9b1ab29c
Sarguru02 <psgnhealth@gmail.com>
2026-01-08 01:30:10 +0530

    markdown render

---
+++ lua/config/plugins/markdown.lua
@@ -0,0 +1,11 @@
+local opts = {}
+
+return {
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+
+	---@module 'render-markdown'
+	---@type render.md.UserConfig
+	opts = opts,
+	lazy = true,
+}
